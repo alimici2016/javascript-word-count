5d3cf987239f015923d663e563f3be70571a28f2
Hamsa Alimci <alimici2016@gmail.com>
2022-01-20 20:44:46 -0600

    The function is now able to receive an argument, when that argument is passed through, there are methods called to try to uniform this (phrase) array

--- word-count.js
+++ word-count.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const countWords = () => {
+export const countWords = (phrase) => {
+  //This first few lines are to make the inputs an array, and uniform
+  //replace tabs and new spaces in the phrase provided
+  const editedPhrase = phrase.replace(/[\n\t]/g, " ");
+  //convert to lowercase
+  const toLowerCase = editedPhrase.toLowerCase();
+  //find certain oddities in string and join them to create substrings accordingly
+  let phraseArray = toLowerCase
+    .split(",")
+    .join("")
+    .split('"')
+    .join("")
+    .split("'")
+    .join("")
+    .split(":")
+    .join("")
+    .split(" ");
+  //check log of final prodcut before looping through
   throw new Error('Remove this statement and implement this function');
 };
