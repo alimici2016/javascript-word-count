71a7ff5ce7aa11e4e66fa47297588b8319239ad2
Hamsa Alimci <alimici2016@gmail.com>
2022-01-20 21:23:21 -0600

    Made a loop within a loop to check if there are multiple occurrences of a word, if there are that count(which is the value of the object) is set to count incremented. Otherwise the object is only initialized with just count which is eqaul to one

--- word-count.js
+++ word-count.js
@@ -5,6 +5,7 @@
 
 export const countWords = (phrase) => {
   //This first few lines are to make the inputs an array, and uniform
+
   //replace tabs and new spaces in the phrase provided
   const editedPhrase = phrase.replace(/[\n\t]/g, " ");
   //convert to lowercase
@@ -20,6 +21,30 @@ export const countWords = (phrase) => {
     .split(":")
     .join("")
     .split(" ");
-  //check log of final prodcut before looping through
+  //check log of final product before looping through
+
+  //loop through array
+  for (let i = 0; i < phraseArray.length; i++) {
+    //create empty object that will be initialized with key value pair as it loops
+    const phraseObj = {};
+    //initialize count for each word
+    let count = 1;
+    //loop again within loop to check if there are multiples of this word
+    for (let j = 0; j < phraseArray.length; j++) {
+      //change both cases to lower case to uniform each index, 
+      //if they are the same only make an object and set the key to the index
+      //and increment the count value
+      //else just make an object the key the index and set the value to count which is just 1
+      if (
+        phraseArray[i].toLowerCase() === phraseArray[j].toLowerCase() &&
+        i !== j
+      ) {
+        phraseObj[phraseArray[i]] = count++;
+      } else {
+        phraseObj[phraseArray[i]] = count;
+      }
+    }
+  }
+
   throw new Error('Remove this statement and implement this function');
 };
